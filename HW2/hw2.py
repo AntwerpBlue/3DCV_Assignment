d53622ae02cee5966b834faa4395ef098e3b3f37
Oren <112540659+AntwerpBlue@users.noreply.github.com>
2024-05-28 20:16:50 +0800

    Update hw2.py

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -3,3 +3,10 @@ from pytorch3d.io import load_obj,save_obj
 
 verts_flame, faces_flame, aux = load_obj("data/flame_model.obj")
 verts_head, faces_head, axu = load_obj("data/head_model.obj")
+
+mean_pos_flame=torch.mean(verts_flame,0)
+mean_pos_head=torch.mean(verts_head,0)
+trans=mean_pos_head-mean_pos_flame
+verts_flame=verts_flame+trans
+
+save_obj("data/flame_model_trans.obj", verts=verts_flame, faces=faces_flame[0])
